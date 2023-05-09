5901d9bb759b1303b8978aa3b253fdffac1b9749
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-05-09 13:05:51 +0530

    Logic for, Getting data from input field to list.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
-function App() {
+import React, { useState } from "react"; //Explanation is at the end of file.
+const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]); //Creating empty array
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItem = () => {
+    setItems((oldItems) => {
+      //getting setItems into oldItems
+      return [...oldItems, inputList]; //making an array and inserting the data
+      //to inputList
+    });
+    setInputList("");
+  };
   return (
     <>
       <div className="main_div">
@@ -6,16 +22,42 @@ function App() {
           <br />
           <h1> ToDo List</h1>
           <br />
-          <input type="text" placeholder="Add a Item" />
-          <button> + </button>
+          <input
+            type="text"
+            placeholder="Add a Item"
+            value={inputList}
+            onChange={itemEvent}
+          />
+          <button onClick={listOfItem}> + </button>
 
           <ol>
-            <li> Apple </li>
+            {/* <li> {inputList} </li>  */}
+            {Items.map((itemval) => {
+              return <li> {itemval} </li>;
+            })}
           </ol>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default App;
+
+// step 1 >
+//jab user input field mai click kare to basicly ak empy array create ho jase
+// or vo show hone k lia call ho jae.
+
+// setp 2 >
+//const [Items, setItems] = useState([]);
+// ham ak empty array create kr rahe hai or oske data ko setItems se use karenge
+
+// const listOfItem = () => {
+//   setItems((oldItems) => {
+//     return [...oldItems, inputList];
+//   });
+//   setInputList("");
+// };
+// issmai (oldItems) oper setItems se aa raha hai, to hm, return bhi ak
+// array kr rahe hai, jismai ... se data, lakr ose inputList mai dal dinge jise ko
+// voha neche update ho jae.
